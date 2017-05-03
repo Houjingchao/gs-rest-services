e044dcb6689764f6d97b0e9fc0380984bf769c8d
hjc <code_writer@163.com>
2017-05-03 22:27:19 +0800

    simple rest

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Hjc on 2017/5/3.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
